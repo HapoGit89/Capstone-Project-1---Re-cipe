1cc02f1ce12bf062fba5128190d4b81a354f6f06
Hannes <hannes.porombka@gmail.com>
2023-05-04 21:24:06 +0200

    debugged favourite add funcs

--- static/app.js
+++ static/app.js
@@ -1,20 +1,34 @@
-stars = document.querySelectorAll('#star');
+const stars = document.querySelectorAll('#star');
 for (let i = 0; i < stars.length; i++) {
     stars[i].addEventListener("click", starClick)
 }
 
 async function starClick(e){
     if (e.target.classList.contains('fa-regular')){
-        console.log(`${e.target.dataset.recipe}`)
-        resp = await axios.post(`/recipes/favourites/${e.target.dataset.recipe}/add`)
-        e.target.classList.remove('fa-regular')
-        e.target.classList.add('fa-solid')
-        return resp
+        try{
+        const resp = await axios.post(`/recipes/favourites/${e.target.dataset.recipe}/add`)
+            e.target.classList.remove('fa-regular')
+            e.target.classList.add('fa-solid')
+            return resp
+        }
+        catch(e) {
+         e.target.classList.remove('fa-solid')
+        e.target.classList.add('fa-regular')
+        }
+        
     }
     else {
-        resp = await axios.post(`/recipes/favourites/${e.target.dataset.recipe}/remove`)
-        e.target.classList.remove('fa-solid')
-        e.target.classList.add('fa-regular')
-        return resp
+        try{
+        const resp = await axios.post(`/recipes/favourites/${e.target.dataset.recipe}/remove`)
+            e.target.classList.remove('fa-solid')
+            e.target.classList.add('fa-regular')
+            return resp
+        }
+        catch(e){
+            e.target.classList.remove('fa-regular')
+            e.target.classList.add('fa-solid')
+        }
+        
+        
     }
 }
